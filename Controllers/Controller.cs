55b41d09988b24dcdc61b0ee2def820c603fd53b
Kriszchan <panyik.krisztian@students.jedlik.eu>
2026-03-20 10:32:47 +0100

    init

---
+++ Controllers/Controller.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace devopsBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LicensePlateController : ControllerBase
+    {
+        private static readonly Random _random = new Random();
+
+        [HttpGet("plates")]
+        public ActionResult<IEnumerable<string>> GetRandomPlates()
+        {
+            var plates = new List<string>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                plates.Add(GenerateHungarianPlate());
+            }
+
+            return Ok(plates);
+        }
+        private string GenerateHungarianPlate()
+        {
+            int num = _random.Next(1, 1000);
+            string numberPart = num.ToString("D3");
+
+            return $"{GenerateRandomLetters(2)} {GenerateRandomLetters(2)}-{numberPart}";
+        }
+
+        private string GenerateRandomLetters(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var result = new StringBuilder();
+            for (int i = 0; i < length; i++)
+            {
+                result.Append(chars[_random.Next(chars.Length)]);
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ Dockerfile
@@ -0,0 +1,25 @@
+# Base image for running the app
+FROM mcr.microsoft.com/dotnet/aspnet:8.0 AS base
+USER $APP_UID
+WORKDIR /app
+EXPOSE 8080
+
+# Build stage
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+ARG BUILD_CONFIGURATION=Release
+WORKDIR /src
+COPY ["devopsBackend.csproj", "."]
+RUN dotnet restore "./devopsBackend.csproj"
+COPY . .
+RUN dotnet build "./devopsBackend.csproj" -c $BUILD_CONFIGURATION -o /app/build
+
+# Publish stage
+FROM build AS publish
+ARG BUILD_CONFIGURATION=Release
+RUN dotnet publish "./devopsBackend.csproj" -c $BUILD_CONFIGURATION -o /app/publish /p:UseAppHost=false
+
+# Final stage
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "devopsBackend.dll"]
