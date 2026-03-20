cc753888d6e1b2a830c4b04801f0fd8e28fecfda
Kriszchan <panyik.krisztian@students.jedlik.eu>
2026-03-20 11:09:08 +0100

    wpdate

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// Configure CORS
+var devFrontendOrigin = "*";
+var corsPolicyName = "_frontend_policy";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", p => p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,6 +23,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Enable CORS (must be before MapControllers / endpoints)
+app.UseCors(corsPolicyName);
 
 app.UseAuthorization();
 
